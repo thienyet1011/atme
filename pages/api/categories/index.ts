5f697e24c9550bc4642ba3c021e464d710c59392
hoanglong1011 <viethq@spaceaa.com>
2021-07-20 15:57:51 +0700

    Edit "api/categories"

--- pages/api/categories/index.ts
+++ pages/api/categories/index.ts
@@ -4,34 +4,41 @@ import Category from '../../../db/model/Category';
 import { CategoryModel } from '../../../model/Category';
 
 export const getCategories = async () => {
-  // var categories: CategoryModel[] = await Category.getCategories();
-  // categories = await Promise.all(categories.map(async (category: CategoryModel) => {
-  //   const subCategories: CategoryModel[] = await Category.getSubCategories(category.id);
-  //   category.children = subCategories;
+  var categories: CategoryModel[] = await Category.getCategories();
+  categories = await Promise.all(categories.map(async (category: CategoryModel) => {
+    const subCategories: CategoryModel[] = await Category.getSubCategories(category.id);
+    category.children = subCategories;
     
-  //   return category;
-  // }));
+    return category;
+  }));
 
-  return [{
-    id: 1,
-    title: 'Mạch điện',
-    alt: 'mach_dien',
-    image: '/categories/EU_PSU_WHITE-800x800.jpg',
-    keywords: '',
-    description: '',
-    parent: 0,
-    children: [],
-  }];
+  return categories;
 }
 
-export default getHandler()
-  .get(async (req, res) => {
-    const categories = await getCategories();
-    res.status(200).json({
-      payload: {
-        success: true,
-        status: 200,
-        categories, 
-      },
-    });
-  })
+export default async function handler(req, res) {
+  switch(req.method) {
+    case "GET":
+      const categories = await getCategories();
+      return res.status(200).json({
+        payload: {
+          success: true,
+          status: 200,
+          categories, 
+        },
+      });
+    default:
+      return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
+  }
+}
+
+// export default getHandler()
+//   .get(async (req, res) => {
+//     const categories = await getCategories();
+//     res.status(200).json({
+//       payload: {
+//         success: true,
+//         status: 200,
+//         categories, 
+//       },
+//     });
+//   })
