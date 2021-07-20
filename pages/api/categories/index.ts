2afc813793039d8324f9c05ea3c412fa820ea4ac
hoanglong1011 <viethq@spaceaa.com>
2021-07-20 15:48:06 +0700

    Edit api/categories

--- pages/api/categories/index.ts
+++ pages/api/categories/index.ts
@@ -4,15 +4,24 @@ import Category from '../../../db/model/Category';
 import { CategoryModel } from '../../../model/Category';
 
 export const getCategories = async () => {
-  var categories: CategoryModel[] = await Category.getCategories();
-  categories = await Promise.all(categories.map(async (category: CategoryModel) => {
-    const subCategories: CategoryModel[] = await Category.getSubCategories(category.id);
-    category.children = subCategories;
+  // var categories: CategoryModel[] = await Category.getCategories();
+  // categories = await Promise.all(categories.map(async (category: CategoryModel) => {
+  //   const subCategories: CategoryModel[] = await Category.getSubCategories(category.id);
+  //   category.children = subCategories;
     
-    return category;
-  }));
+  //   return category;
+  // }));
 
-  return categories;
+  return [{
+    id: 1,
+    title: 'Mạch điện',
+    alt: 'mach_dien',
+    image: '/categories/EU_PSU_WHITE-800x800.jpg',
+    keywords: '',
+    description: '',
+    parent: 0,
+    children: [],
+  }];
 }
 
 export default getHandler()
