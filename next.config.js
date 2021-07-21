8b6b481633adacc34c5ff17582bf539f47c8957e
hoanglong1011 <viethq@spaceaa.com>
2021-07-21 10:48:34 +0700

    Edit next.config.js environment variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports =  {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   // Supported targets are "serverless" and "experimental-serverless-trace"
@@ -6,4 +6,13 @@ module.exports = {
   // Webpack 5 is enabled by default
   // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
   webpack5: true,
+  env: {
+    ROWS_PER_PAGE: process.env.ROWS_PER_PAGE,
+    BASEURL:  process.env.BASEURL,
+    DATABASE_HOST:  process.env.DATABASE_HOST,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    DATABASE_USERNAME: process.env.DATABASE_USERNAME,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_PORT: process.env.DATABASE_PORT,
+  }
 };
